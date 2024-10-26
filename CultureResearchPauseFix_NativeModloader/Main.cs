110763d1d908d93f4b2607f28c78e6b25e0e2bf7
Jean-Antoine d'Anglerais <zzzzzzzorro@mail.ru>
2024-10-26 11:06:23 +0300

    Добавьте файлы проекта.

---
+++ CultureResearchPauseFix_NativeModloader/Main.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
+
+namespace CultureResearchPauseFix_NativeModloader
+{
+    internal class Main : MonoBehaviour
+    {
+        public static Harmony harmony = new Harmony(MethodBase.GetCurrentMethod().DeclaringType.Namespace);
+        private bool _initialized = false;
+
+        public void Update()
+        {
+            if (global::Config.gameLoaded && !_initialized)
+            {
+                harmony.Patch(AccessTools.Method(typeof(CultureManager), nameof(CultureManager.updateProgress)), transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.updateProgress_Transpiler))));
+
+                harmony.Patch(AccessTools.Method(typeof(CultureManager), nameof(CultureManager.updateRecalcValues)), transpiler: new HarmonyMethod(AccessTools.Method(typeof(Patches), nameof(Patches.updateRecalcValues_Transpiler))));
+
+                _initialized = true;
+            }
+        }
+    }
+}
---
+++ CultureResearchPauseFix_NativeModloader/Patches.cs
@@ -0,0 +1,50 @@
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+
+namespace CultureResearchPauseFix_NativeModloader
+{
+    public static class Patches
+    {
+        public static IEnumerable<CodeInstruction> updateProgress_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            var label = generator.DefineLabel();
+
+            var newCodes = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(World), "get_world")),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(MapBox), "isPaused")),
+                new CodeInstruction(OpCodes.Brtrue, label)
+            };
+
+            codes.InsertRange(0, newCodes);
+
+            codes.Last().labels.Add(label);
+
+            return codes.AsEnumerable();
+        }
+
+        public static IEnumerable<CodeInstruction> updateRecalcValues_Transpiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
+        {
+            var codes = new List<CodeInstruction>(instructions);
+
+            var label = generator.DefineLabel();
+
+            var newCodes = new List<CodeInstruction>
+            {
+                new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(World), "get_world")),
+                new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(MapBox), "isPaused")),
+                new CodeInstruction(OpCodes.Brtrue, label)
+            };
+
+            codes.InsertRange(0, newCodes);
+
+            codes.Last().labels.Add(label);
+
+            return codes.AsEnumerable();
+        }
+    }
+}
